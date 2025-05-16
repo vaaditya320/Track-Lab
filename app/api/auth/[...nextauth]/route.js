eb15ba86394446f4740ba2dcb1e9be449fc59f30
Aaditya Vinayak <vaaditya320@gmail.com>
2025-05-16 21:37:34 +0530

    Update Google OAuth parameters to prompt for account selection and no consent

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,9 +9,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
+          prompt: "select_account"
         },
       },
     }),
